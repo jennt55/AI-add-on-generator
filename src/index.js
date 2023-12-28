ba3baee0c67745995a5f60626873e898ac812fa0
jennt55 <146074617+jennt55@users.noreply.github.com>
2023-12-28 14:52:41 +0000

    adding poem so far

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
+  let instructionInput = document.querySelector("instructions-input");
+  let apiKey = "331fct734c14fe46300adod3e6c6aacb";
+  let context =
+    "You are a romantic poem expert and love to write short poems. Your mission is to write a four line poem in basic english.";
+  let prompt = `generate a poem about ${instructionInput.value} `;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
   let poemElement = document.querySelector("#poem");
-  poemElement.innerHTML = "The poem goes here";
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
